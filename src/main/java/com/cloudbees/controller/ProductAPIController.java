0196b2c83bfef93b313934403bf279425a118dad
Sindhap <101865238+Sindhap@users.noreply.github.com>
2024-01-25 12:20:59 +0530

    Update ProductAPIController.java

--- src/main/java/com/cloudbees/controller/ProductAPIController.java
+++ src/main/java/com/cloudbees/controller/ProductAPIController.java
@@ -6,11 +6,9 @@ import javax.annotation.Generated;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-01-25T10:40:44.336476700+05:30[Asia/Calcutta]")
 @Controller
-@RequestMapping("${openapi.product.base-path:}")
 public class ProductAPIController implements ProductAPI {
 
     private final ProductAPIDelegate delegate;
